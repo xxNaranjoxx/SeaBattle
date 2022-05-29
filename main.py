b0ea9d22174947411f5a22c56fa2d0c5994b40b4
xxNaranjoxx <snaranjo@estudiantec.cr>
2022-05-28 19:56:38 -0600

    Añadi salon de la fama

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import tkinter
 from tkinter import *
+from tkinter import messagebox
+
+bubbleGum = "#FFBED2"
+lightBlue = "#98D3E3"
 
 def main():
 #Se crea ventana
@@ -7,27 +11,71 @@ def main():
    ventana.title("Monkey")
    ventana.geometry("800x600")
    ventana.config(bg="white")
-   ventana.resizable(False, False) #No se puede hacer grande ni pequeño
+   ventana.resizable(False, False) #Tamaño
 
-# Se crea Canva
-   my_canvas = Canvas(ventana, width=800, height=600)
-   my_canvas.pack(fill="both", expand=True)
+   # Se crea Canva
+   splashCanva = Canvas(ventana, width=800, height=600, borderwidth=0, highlightthickness=0, bg=lightBlue)
+   splashCanva.place(x=0, y=0)
+
+   fondoRegistro = PhotoImage(file="Computador.png")
+   splashCanva.create_image(0, 0, image=fondoRegistro, anchor="nw")
 
 
-   CampoJugador = PhotoImage(file="Principal.png")
-   my_canvas.create_image(0, 0, image=CampoJugador , anchor="nw")
+
+
+   def registro():
+      my_canvas = Canvas(ventana, width=800, height=600, borderwidth=0, highlightthickness=0, bg=lightBlue)
+      my_canvas.place(x=0, y=0)
+
+      fondoRegistro = PhotoImage(file="Registro.png")
+      my_canvas.create_image(0, 0, image=fondoRegistro, anchor="nw")
+
+      nombres = Entry(my_canvas,width=50,borderwidth=3)
+      nombres.place(x=195,y=290)
+      #Juego
+
+
+      def registroJug():
+         if nombres.get() == "":
+            messagebox.showwarning("Nombre", "Error en el nombre")
+         else:
+            messagebox.showinfo("Nombre", "Registrado")
+            archivo = open("Jugadores1.txt", "a")
+            archivo.write(nombres.get() + "-")
+            archivo.close()
+            salonFama()
+
+
+      imprimirJugador = Button(my_canvas, text="Registrar y Jugar", command=registroJug) # Regsitrar y Jugar
+      imprimirJugador.place(x=195, y=335)
+
+
+
+
 
    def salonFama():
 
       # Canva Registro
-      canvaJuego = tkinter.Canvas(ventana, width=800, height=600, borderwidth=0, highlightthickness=0, bg="lightYellow")
+      canvaJuego = tkinter.Canvas(ventana, width=800, height=600, borderwidth=0, highlightthickness=0, bg=bubbleGum)
       canvaJuego.place(x=0, y=0)
 
- #Después de 5 segundos se abre el registro del jugador
-   ventana.after(5000,salonFama)
+      regresoB = Button(canvaJuego, text="Regresar al registro", command=registro)  # Salon de la fama
+      regresoB.place(x=5, y=5)
 
-   # Loop de la ventana para que se repinte
-   ventana.mainloop()
 
-#Se llama el main para que se pueda ejecutar
-main()
+   def ayuda():
+      canvaAyuda = tkinter.Canvas(ventana, width=800, height=600, borderwidth=0, highlightthickness=0, bg="lightYellow")
+      canvaAyuda.place(x=0, y=0)
+
+
+   salonFamaB = Button(my_canvas, text="Salon de la fama", command=salonFama)  # Salon de la fama
+   salonFamaB.place(x=5, y=5)
+   ayudaB = Button(my_canvas, text="Ayuda", command=ayuda)  # Ayuda
+   ayudaB.place(x=110, y=5)
+
+   ventana.after(5000,registro)
+
+   ventana.mainloop() # loop para main
+
+
+main()# Llamado de main
